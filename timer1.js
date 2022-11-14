a1d0ae79d23696e4e193f6df7cdc0fa150562f3b
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-13 18:05:43 -0800

    add condition to break function if input is NaN or negative

--- timer1.js
+++ timer1.js
@@ -2,6 +2,7 @@ const cli = process.argv.slice(2);
 
 const timer = (time) => {
   for (const value of time) {
+    if (Number(value) < 0 || isNaN(Number(value))) break;
     setTimeout(() => {
       process.stdout.write('\x07');
       console.log(`BEEP after ${value} seconds!`);
